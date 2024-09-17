448fa73855249887145e998fa56b895fdcffea31
Max van den Hoven <79513114+maxvandenhoven@users.noreply.github.com>
2024-09-17 09:25:26 +0200

    Initial commit

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,63 @@
+import os
+import re
+import shutil
+
+
+def fix_whitespace(filename: str) -> None:
+    """Fix whitespace from Jinja2 template generated file
+
+    Notes
+    -----
+    - Fixes whitespace when intermediate if-templates insert newlines when not active.
+    - Fixes trailing whitespace.
+
+    Parameters
+    ----------
+    filename : str
+        Relative path of file to fix (relative to generated project root)
+    """    
+    with open(filename, "r") as file:
+        content = file.read()
+        
+    content = re.sub(r"\n{3,}", "\n\n", content)
+    
+    with open(filename, "w") as file:
+        file.write(content.strip())
+
+
+def remove_mlflow_folder() -> None:
+    """Remove mlflow folder from generated project root"""
+    shutil.rmtree("./mlflow/")
+
+
+def remove_tests_folder() -> None:
+    """Remove tests folder from generated project root"""
+    shutil.rmtree("./tests/")
+
+
+def remove_pyproject_toml_file() -> None:
+    """Remove pyproject.toml file from generated project root"""
+    os.remove("./pyproject.toml")
+
+
+def main() -> None:
+    """Perform post-generation cleanup based on Cookiecutter template variables"""
+    # Fix whitespace for all files that use Jinja2 templates.
+    fix_whitespace("./Makefile")
+    fix_whitespace("./docker/.env")
+    fix_whitespace("./docker/docker-compose.yml")
+    fix_whitespace("./{{ cookiecutter.__package_name }}/Dockerfile")
+    fix_whitespace("./{{ cookiecutter.__package_name }}/requirements.txt")
+
+    # Remove MLFlow folder if unused.
+    if "{{ cookiecutter.experiment_tracker }}" != "mlflow":
+        remove_mlflow_folder()
+
+    # Remove tests folder and devtools configuration file if unused.
+    if "{{ cookiecutter.setup_devtools }}" != "true":
+        remove_tests_folder()
+        remove_pyproject_toml_file()
+
+
+if __name__ == "__main__":
+    main()
